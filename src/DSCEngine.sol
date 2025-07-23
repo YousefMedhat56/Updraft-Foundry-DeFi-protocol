eff43d280988fab8f87109277acb68667498e7e8
Yousef Medhat <yousef.medhat564@gmail.com>
2025-07-23 22:19:06 +0300

    feat(DSCEngine): setup

---
+++ src/DSCEngine.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract DSCEngine {
+    ///////////////////
+    //   Functions   //
+    ///////////////////
+    constructor() {}
+
+    ///////////////////////////
+    //   External Functions  //
+    ///////////////////////////
+    function depositCollateralAndMintDsc() external {}
+
+    function depositCollateral() external {}
+
+    function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDsc() external {}
+
+    function burnDsc() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
+}
