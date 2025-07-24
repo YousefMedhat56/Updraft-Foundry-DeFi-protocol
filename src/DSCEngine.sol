3ad1a47207e336c8758e496e632830507a5a838b
Yousef Medhat <yousef.medhat564@gmail.com>
2025-07-24 11:33:57 +0300

    feat(DSCEngine): implement depositCollateral function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,8 +2,10 @@
 pragma solidity 0.8.19;
 
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {IERC20} from "@openzepplin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzepplin-contracts/contracts/security/ReentrancyGuard.sol";
 
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
     ///////////////////
     //   Errors      //
     ///////////////////
@@ -11,12 +13,38 @@ contract DSCEngine {
     error DSCEngine__TokensAndPriceFeedsLengthIsZero();
     error DSCEngine__AddressZero();
     error DSCEngine__TokenAlreadyConfigured();
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__InvalidToken();
+    error DSCEngine__TransferFailed();
 
     /////////////////////////
     //   State Variables   //
     /////////////////////////
-    mapping(address token => address priceFeed) private s_priceFeeds;
     DecentralizedStableCoin private immutable i_dsc;
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address collatralToken => uint256 amount)) s_collateralBalances;
+
+    ///////////////////
+    //   Events      //
+    ///////////////////
+    event CollateralDeposited(address indexed user, address indexed collateralToken, uint256 collateralAmount);
+
+    ///////////////////
+    //   Modifiers   //
+    ///////////////////
+    modifier moreThanZero(uint256 val) {
+        if (val <= 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isValidToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__InvalidToken();
+        }
+        _;
+    }
 
     ///////////////////
     //   Functions   //
@@ -33,11 +61,7 @@ contract DSCEngine {
      *      - A token is already configured with a price feed.
      * @dev TODO: The constructor does not verify that `tokens` are valid ERC20 contracts or that `priceFeeds` are valid Chainlink price feeds.
      */
-    constructor(
-        address[] memory tokens,
-        address[] memory priceFeeds,
-        address dscAddress
-    ) {
+    constructor(address[] memory tokens, address[] memory priceFeeds, address dscAddress) {
         if (tokens.length != priceFeeds.length) {
             revert DSCEngine__TokensAndPriceFeedsMustBeTheSameLength();
         }
@@ -67,7 +91,27 @@ contract DSCEngine {
     ///////////////////////////
     function depositCollateralAndMintDsc() external {}
 
-    function depositCollateral() external {}
+    /**
+     * @notice Deposits collateral tokens into the DSCEngine contract.
+     * @param collateralTokenAddress The address of the ERC20 token (e.g., WETH or WBTC).
+     * @param collateralAmount The amount of tokens to deposit (in token decimals).
+     * @dev TODO: Requires the user to have approved the DSCEngine contract to spend `collateralAmount` tokens.
+     * @dev Emits a `CollateralDeposited` event on success.
+     */
+    function depositCollateral(address collateralTokenAddress, uint256 collateralAmount)
+        external
+        isValidToken(collateralTokenAddress)
+        moreThanZero(collateralAmount)
+        nonReentrant
+    {
+        s_collateralBalances[msg.sender][collateralTokenAddress] += collateralAmount;
+        emit CollateralDeposited(msg.sender, collateralTokenAddress, collateralAmount);
+
+        bool success = IERC20(collateralTokenAddress).transferFrom(msg.sender, address(this), collateralAmount);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
 
     function redeemCollateralForDsc() external {}
 
